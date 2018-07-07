9bdfc8be85175c563ddaed2df6502a0f813638e9
Michael Andersson <m-andersson@outlook.com>
2018-07-07 19:40:22 +0200

    Added new executable to CmakeList

---
+++ client/client.c
@@ -0,0 +1,12 @@
+//
+// Created by njia on 2018-07-07.
+//
+
+#include "client.h"
+#include <stdio.h>
+
+int main(int argc, char **argv){
+  printf("Tjoo heeej! \n");
+
+  return 0;
+}
---
+++ client/client.h
@@ -0,0 +1,8 @@
+//
+// Created by njia on 2018-07-07.
+//
+
+#ifndef DODOU2_CLIENT_H
+#define DODOU2_CLIENT_H
+
+#endif //DODOU2_CLIENT_H
